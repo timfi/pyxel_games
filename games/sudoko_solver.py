19b08c9de616fa73083362ee1551f87ae705d96c
tim-fi <tim.fischer98@hotmail.com>
2019-11-11 12:34:56 +0100

    cleaned up sudoku-solver a bit more and added some docstrings to it

--- games/sudoko_solver.py
+++ games/sudoko_solver.py
@@ -63,6 +63,7 @@ SQR_IDX = cast(BoardIdxSet, tuple(cast(BoardObj, tuple(idxs)) for idxs in _SQR_I
 # -------------------------------------------------------
 @dataclass(frozen=True)
 class Board:
+    """Representation of a soduko riddle and its state during a solve."""
     _data: BoardData = field(default_factory=GEN_EMPTY_BOARD)
     _locked: BoardLock = field(default_factory=GEN_EMPTY_LOCKS)
 
@@ -76,42 +77,57 @@ class Board:
             raise KeyError(f"Index out of scope. {idx}")
 
     def set(self, idx: int, val: int) -> Board:
+        """Get a new board with a given cell set to a given value.
+
+        This will return the current board if the given arguments
+        don't comply with the current riddle. 
+        """
+        if self.is_locked(idx) or 0 > val or val > 9:
+            return self
         data = list(self._data)
         data[idx] = val
         return Board(tuple(data), self._locked)
 
     def is_locked(self, idx: int):
+        """Check if a cell is part of the riddle."""
         return self._locked[idx]
 
     def freeze(self) -> Board:
+        """Make riddle out of all currently filled cells."""
         return Board(self._data, tuple(
             cell != 0
             for cell in self._data
         ))
 
     def clear(self) -> Board:
+        """Get a unfilled copy of the current riddle."""
         return Board(tuple(
             cell if locked else 0
             for cell, locked in zip(self._data, self._locked)
         ), self._locked)
 
     def _iter(self, idx_set: BoardIdxSet) -> Iterator[BoardObj]:
+        """Get all board objects definied by a given board index set."""
         yield from (cast(BoardObj, tuple(self._data[idx] for idx in idxs)) for idxs in idx_set)
 
     @property
     def rows(self) -> Iterator[BoardObj]:
+        """Get all rows."""
         yield from self._iter(ROW_IDX)
 
     @property
     def cols(self) -> Iterator[BoardObj]:
+        """Get all columns."""
         yield from self._iter(COL_IDX)
 
     @property
     def sqrs(self) -> Iterator[BoardObj]:
+        """Get all squares."""
         yield from self._iter(SQR_IDX)
 
     @property
     def mistakes(self) -> Tuple[Tuple[int, ...], Tuple[int, ...], Tuple[int, ...]]:
+        """Get rows, columns, and squares that violate the puzzle constraints."""
         return (
             tuple([i for i, row in enumerate(self.rows) if sum(set(row)) != sum(row)]),
             tuple([i for i, col in enumerate(self.cols) if sum(set(col)) != sum(col)]),
@@ -120,27 +136,40 @@ class Board:
 
     @property
     def valid(self) -> bool:
+        """Check if the current state of the riddle is valid."""
         return all(len(group) == 0 for group in self.mistakes)
 
     @property
     def done(self) -> bool:
+        """Check if the riddle is completed."""
         return sum(self._data) == 405 and self.valid
 
     @property
     def to_fill(self):
+        """Get the amount of fillable cells that aren't filled."""
+        return sself.fillable - self.filled
+
+    @property
+    def filled(self):
+        """Get the amount of fillable cells that are filled."""
         return sum(
-            not locked and cell == 0
+            not locked and cell != 0
             for cell, locked in zip(self._data, self._locked)
         )
 
-    def diff(self, other: Board) -> int:
-        return sum(
-            abs(a - b)
-            for a, b in zip(self, other)
-        )
+    @property
+    def fillable(self):
+        """Get the amount of fillable cells."""
+        return 81  - sum(self._locked)
+
+    def occurences(self, val: int) -> float:
+        return sum(val == i for i in self._data) / 9
 
     @property
     def candidates(self) -> Tuple[(Set[int],)*81]:
+        """
+        Get the entry candidates for each cell.
+        """
         M = set(range(1, 10))
         row_idx = lambda idx: idx // 9
         col_idx = lambda idx: idx % 9
@@ -151,9 +180,11 @@ class Board:
         sqrs = list(map(set, self.sqrs))
 
         return tuple(
-            M - (cols[col_idx(i)] | rows[row_idx(i)] | sqrs[sqr_idx(i)])
-            if not self.is_locked(i) else
-            set()
+            (
+                M - (cols[col_idx(i)] | rows[row_idx(i)] | sqrs[sqr_idx(i)])
+                if not self.is_locked(i) else
+                set()
+            )
             for i in range(81)            
         )
 
@@ -165,23 +196,34 @@ class Board:
 # -------------------------------------------------------
 @dataclass
 class Solver:
+    """Sudoko solver
+
+    This is a sudoko solver that makes use of a stack
+    of generators to store intermediate execution states.
+    This allows the solver to be executed step by step
+    and paused after any iteration.
+    """
     _running: bool = field(init=False, default=False)
     _stack: List = field(init=False, default_factory=list)
     _checked_boards: List = field(init=False, default_factory=list)
 
-    def start(self, board: Board, reset_after: int = -1):
-        self._stack = [self._solve(board)]
+    def start(self, board: Board):
+        """Initialize solver with given board."""
+        self._stack = [self._next(board)]
         self._checked_boards = []
         self._running = True
 
     def stop(self):
+        """Halt solver."""
         self._running = False
 
     @property
     def running(self):
+        """Check if the solver is currently running"""
         return self._running
     
-    def update(self) -> Board:
+    def next(self) -> Board:
+        """Execute the next step of the solver."""
         if not self._stack:
             return None
 
@@ -194,40 +236,49 @@ class Solver:
         if board.done:
             self._running = False
         elif follow:
-            self._stack.append(self._solve(board))
+            self._stack.append(self._next(board))
         else:
             self._stack.pop(-1)
         return board
 
-    def _solve(self, board: Board) -> Iterator[Tuple[Board, int]]:
-        idx = float('inf')
-        candidates = set(range(1, 10))
-        importance = float('-inf')
+    def _next(self, board: Board) -> Iterator[Tuple[Board, int]]:
+        """Get prefered child-states for given board."""
+        idx, candidates = self._get_preffered_cell(board)
 
-        for _idx, _candidates in enumerate(board.candidates):
-            if board[_idx] == 0:
-                if len(_candidates) == 0:
-                    yield False, board
-                elif (
-                    len(_candidates) < len(candidates) and
-                    importance < (_importance := -sum(c == i for c in _candidates for i in board))
-                ):
-                    candidates = _candidates
-                    idx = _idx
-                    importance = _importance
+        if not candidates:
+            yield False, board
 
-        for candidate in sorted(candidates, key=lambda c: -sum(c == i for i in board)):
+        for candidate in sorted(candidates, key=board.occurences):
             _board = board.set(idx, candidate)
             if _board not in self._checked_boards:
                 yield True, _board
                 self._checked_boards.append(_board)
         yield False, board
 
+    def _get_preffered_cell(self, board: Board) -> Tuple[int, Set[int]]:
+        """Get prefered cell and its candidates for a given board."""
+        all_candidates = board.candidates
+        idx = min(
+            (idx for idx, cell in enumerate(board) if cell == 0),
+            key=lambda idx: (
+                len(candidates := all_candidates[idx]),
+                sum(board.occurences(c) for c in candidates)
+            )
+        )
+        return idx, all_candidates[idx]
+
+
 
 # -------------------------------------------------------
 # Utils
 # -------------------------------------------------------
 def generate_riddle(fill: float = 0.5) -> Board:
+    """Generate a sudoko riddle.
+    
+    This generates a new riddle by filling an empty board using
+    simple bruteforce-backtracking and then randomly removing
+    some cells based on the given fill percentage.
+    """
     def _fill(board: Board, idx: int = 0) -> Optional[Board]:
         if idx >= 80:
             return board
@@ -348,12 +399,22 @@ class App:
     def draw_ui(self):
         pyxel.text(
             BOARD_WIDTH + 5, 2,
-            f"step:    {self._iterations}",
+            f"iter:    {self._iterations: >5d}",
             5
         )
         pyxel.text(
-            BOARD_WIDTH + 5, 1 * pyxel.FONT_HEIGHT + 6,
-            f"infill:  {self._fill:.0%}",
+            BOARD_WIDTH + 5, pyxel.FONT_HEIGHT + 4,
+            f"fill:    {self.board.filled: >2d}/{self.board.fillable: >2d}",
+            5
+        )
+        pyxel.text(
+            BOARD_WIDTH + 5, 2 * pyxel.FONT_HEIGHT + 6,
+            f"          {self.board.filled/self.board.fillable: >4.0%}",
+            5
+        )
+        pyxel.text(
+            BOARD_WIDTH + 5, 3 * pyxel.FONT_HEIGHT + 10,
+            f"infill:  {self._fill: >5.0%}",
             5 + 2 * (not self.solver.running)
         )
 
@@ -365,7 +426,7 @@ class App:
 
     def update(self):
         if self.solver.running:
-            board = self.solver.update()
+            board = self.solver.next()
             if pyxel.btnp(pyxel.KEY_SPACE) or board is None:
                 if board is None:
                     print("Failed to find solution...")
